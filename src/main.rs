cce0f437b7f0b05ead748723b81397150f38ba03
mdx97 <mathewhorner456@gmail.com>
2021-06-06 11:25:40 -0500

    Made bottom-left to top-right diagonal detection work for arbitrary number of squares.

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,15 @@ const PLAYING_AREA_OFFSET: u32 = BORDER_THICKNESS as u32 * 2;
 /// The height and width of the playing area, in pixels.
 const PLAYING_AREA_SIZE: u32 = WINDOW_SIZE - (PLAYING_AREA_OFFSET * 2);
 
-const SQUARE_WIDTH: u32 = 4;
+/// The number of squares in the horizontal and vertical direction.
+/// Total number of squares in the playing area = SQUARES^2
+const SQUARES: u32 = 4;
 
 /// The height and width of each square, in pixels.
-const SQUARE_SIZE: u32 = PLAYING_AREA_SIZE / SQUARE_WIDTH;
+const SQUARE_SIZE: u32 = PLAYING_AREA_SIZE / SQUARES;
 
 /// How many extra pixels the border must fill in so there is no space between the outer squares and the border.
-const FILL_IN: u32 = PLAYING_AREA_SIZE - (SQUARE_SIZE * SQUARE_WIDTH);
+const FILL_IN: u32 = PLAYING_AREA_SIZE - (SQUARE_SIZE * SQUARES);
 
 #[derive(Clone, PartialEq)]
 enum Square {
@@ -45,12 +47,12 @@ fn fill_rectangle(canvas: &mut WindowCanvas, rectangle: Rect, color: Color) {
 fn get_square_from_coords(x: i32, y: i32) -> Option<usize> {
     let x = x - PLAYING_AREA_OFFSET as i32;
     let y = y - PLAYING_AREA_OFFSET as i32;
-    if x <= 0 || y <= 0 || x >= (SQUARE_SIZE * SQUARE_WIDTH) as i32 || y >= (SQUARE_SIZE * SQUARE_WIDTH) as i32 {
+    if x <= 0 || y <= 0 || x >= (SQUARE_SIZE * SQUARES) as i32 || y >= (SQUARE_SIZE * SQUARES) as i32 {
         return None;
     }
     let col = x as u32 / SQUARE_SIZE;
     let row = y as u32 / SQUARE_SIZE;
-    Some(((row * SQUARE_WIDTH) + col) as usize)
+    Some(((row * SQUARES) + col) as usize)
 }
 
 /// Returns a new rect that covers the inner portion of the given rectangle.
@@ -65,12 +67,12 @@ fn get_inner_rect(rect: Rect) -> Rect {
 
 /// Returns whether or not the given board state has a winner.
 fn has_winner(squares: &Vec<Square>) -> bool {
-    for i in 0..SQUARE_WIDTH as usize {
+    for i in 0..SQUARES as usize {
         if line_has_winner(&squares, |constant, i| (constant, i), i) || line_has_winner(&squares, |constant, i| (i, constant), i) {
             return true;
         }
     }
-    line_has_winner(&squares, |_, i| (i, i), 0) || line_has_winner(&squares, |_, i| (2 - i, i), 0)
+    line_has_winner(&squares, |_, i| (i, i), 0) || line_has_winner(&squares, |_, i| (SQUARES as usize - i - 1, i), 0)
 }
 
 /// Returns whether or not the given line has a winner.
@@ -82,7 +84,7 @@ fn line_has_winner(squares: &Vec<Square>, get_square: fn(usize, usize) -> (usize
     if *line_square == Square::Empty {
         return false;
     }
-    for i in 1..SQUARE_WIDTH as usize {
+    for i in 1..SQUARES as usize {
         let square = get_square(constant, i);
         if *get_square_flatten_index(squares, square.0, square.1) != *line_square {
             return false;
@@ -93,7 +95,7 @@ fn line_has_winner(squares: &Vec<Square>, get_square: fn(usize, usize) -> (usize
 
 /// Returns a square from the square vector by treating it as a table.
 fn get_square_flatten_index(squares: &Vec<Square>, row: usize, col: usize) -> &Square {
-    &squares[(row * SQUARE_WIDTH as usize) + col]
+    &squares[(row * SQUARES as usize) + col]
 }
 
 fn main() {
@@ -115,7 +117,7 @@ fn main() {
         WINDOW_SIZE - (BORDER_THICKNESS as u32 * 4) - FILL_IN,
     );
 
-    let mut squares = vec![Square::Empty; (SQUARE_WIDTH * SQUARE_WIDTH) as usize];
+    let mut squares = vec![Square::Empty; (SQUARES * SQUARES) as usize];
     let mut turn = true;
 
     loop {
@@ -138,7 +140,7 @@ fn main() {
         }
 
         if has_winner(&squares) || !squares.iter().any(|s| *s == Square::Empty) {
-            squares = vec![Square::Empty; (SQUARE_WIDTH * SQUARE_WIDTH) as usize];
+            squares = vec![Square::Empty; (SQUARES * SQUARES) as usize];
             turn = true;
         }
 
@@ -146,8 +148,8 @@ fn main() {
         fill_rectangle(&mut canvas, border_rect, Color::WHITE);
         fill_rectangle(&mut canvas, playing_area_rect, Color::BLACK);
 
-        for i in 0..SQUARE_WIDTH as usize {
-            for j in 0..SQUARE_WIDTH as usize {
+        for i in 0..SQUARES as usize {
+            for j in 0..SQUARES as usize {
                 let rect = Rect::new((PLAYING_AREA_OFFSET + (SQUARE_SIZE * i as u32)) as i32, (PLAYING_AREA_OFFSET + (SQUARE_SIZE * j as u32)) as i32, SQUARE_SIZE, SQUARE_SIZE);
                 canvas.set_draw_color(Color::WHITE);
                 canvas.draw_rect(rect).unwrap();
